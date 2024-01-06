a2bf7ef66dc5296019a8320a2ac40c21b111497b
Bintaaaa <bijantium.yt.32@gmail.com>
2024-01-06 22:58:10 +0700

    feat: add product detail module

---
+++ features/productDetail/src/commonMain/kotlin/com/example/productdetail/ProductDetail.kt
@@ -0,0 +1,12 @@
+package com.example.productdetail
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ProductDetail(name: String){
+    Text(
+        text = name,
+    )
+}
