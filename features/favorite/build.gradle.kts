5935013254c60b88c8fa53f708258785d72e7d71
Bintaaaa <bijantium.yt.32@gmail.com>
2024-01-27 19:31:19 +0700

    feat : bottom nav bar

---
+++ features/favorite/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "favorite"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+
+            implementation(projects.apis.product)
+            implementation(projects.libraries.core)
+            implementation(projects.libraries.components)
+
+            implementation(libs.paging.cashapp.common)
+            implementation(libs.paging.cashapp.compose)
+        }
+
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.paging.androidx.runtime)
+            implementation(libs.paging.androidx.compose)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.bintaaaa.features.favorite"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
+dependencies {
+    implementation(libs.androidx.material3)
+}
